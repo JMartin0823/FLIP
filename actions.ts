57490315018d9d7e55b06e58d790171ef72dca33
S1LB4 <113909639+S1LB4@users.noreply.github.com>
2023-06-04 14:24:45 -0500

    Add files via upload

---
+++ actions.ts
@@ -0,0 +1,35 @@
+import { Screens } from "../types/store";
+import PostsService from '../services/posts'
+import { AddPostAction, GetPostsAction, PostsActions } from "../types/store"
+
+
+export const navigate = (screen: Screens) => {
+  return {
+    type: "NAVIGATE",
+    payload: screen,
+  };
+};
+
+export const setUserCredentials = (user: string) => {
+  return {
+    type: "SETUSER",
+    payload: user,
+  };
+};
+
+
+
+export const getPosts = async (): Promise<GetPostsAction> => {
+  const trips = await PostsService.get();
+  return {
+      action: PostsActions.GET,
+      payload: trips
+  }
+}
+
+export const addNewTrip = ({payload}: Pick<AddPostAction, "payload">): AddPostAction => {
+  return {
+      action: PostsActions.ADD,
+      payload
+  }
+}
---
+++ posts.ts
@@ -0,0 +1,11 @@
+export interface Post {
+    idPost: string;
+    photoURL: string;
+    caption: string;
+}
+export interface Post {
+    caption: string;
+    url: string;
+    btn_text: string;
+    text: string;
+}
