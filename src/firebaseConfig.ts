81e64fe38d717917fe2c4dcdb3d2eba261e7eb97
Juan Martin Lopez <115959645+JMartin0823@users.noreply.github.com>
2023-06-06 11:10:58 -0500

    Add files via upload

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAOTagf8nveSkuW4zJLkIZLqDgR8icc56M",
+    authDomain: "flip-78493.firebaseapp.com",
+    projectId: "flip-78493",
+    storageBucket: "flip-78493.appspot.com",
+    messagingSenderId: "1030131409092",
+    appId: "1:1030131409092:web:1ea2445165c75303d9e991",
+    measurementId: "G-268MKT5Z0E"
+  };
